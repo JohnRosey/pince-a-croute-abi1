ec7fbe499847151a56ffa6a09e8356d97d92dca1
Gansonre Ismael <proismaelgans2017@gmail.com>
2022-09-29 14:13:15 +0000

    add query 2

--- database.cs
+++ database.cs
@@ -71,7 +71,21 @@ ON B.Fonction='PINCE À CROUTE' and A.fonction='PINCE À CROUTE' WHERE [insert_t
 and B.Nom_Emplacement=A.Nom_Emplacement    
 ORDER BY Emplacement ,detection_id desc";
 
-            var cmd = new SqlCommand(query, SQLConnection);
+            var query2 = @" 
+          DECLARE @yesterday DATETIME
+    = DATEADD(DAY, -1, CAST(GETDATE() AS DATE));
+DECLARE @today DATETIME = CAST(GETDATE() AS DATE);
+SELECT D.[detection_id]
+      ,D.[reader_uwb_id]
+      ,D.[tag_id]
+      ,D.[tag_temperature]
+      ,D.[distance]
+      ,D.[insert_timestamp]
+	 
+FROM [ABI-MES-SQL-CL1.APM.ALCOA.COM].[RFID_SURAL].[dbo].[noovelia_kencee_detection] as D
+  WHERE (D.[insert_timestamp] >= @yesterday +'08:00:00.000' and  D.[insert_timestamp]<@today +'08:00:00.000')  and (D.distance BETWEEN  1.88 and 15.82 ) 
+ORDER BY D.detection_id Asc  ";
+            var cmd = new SqlCommand(query2, SQLConnection);
             try
             {
                 SQLConnection.Open();
